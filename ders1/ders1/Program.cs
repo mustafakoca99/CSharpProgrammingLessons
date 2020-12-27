dc980c03cb6e54d7b3314af77e41c053971e52b9
Mustafa Koca <54147948+mustafakoca99@users.noreply.github.com>
2020-12-27 16:39:22 +0300

    CSharpProgrammingLessons
    c # lessons will be shared. If you follow, you can be informed about the current contents.

---
+++ ders1/ders1/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ders1
+{
+    //sdfsdfsd
+    /*
+     * asdas*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("merhaba dunya!!!");
+            //Console.WriteLine("mustafa koca");
+            //Console.ReadKey();
+            //Console.ReadLine();
+            /*string mustafa;
+            Console.WriteLine("kelime gir: ");
+            mustafa = Console.ReadLine();
+            Console.WriteLine(mustafa);
+            Console.ReadLine();*/
+            //string mustafa;
+            //Console.WriteLine("kelime gir: ");
+            //mustafa = Console.ReadLine();
+            //string koca = mustafa.Length.ToString();
+            //Console.WriteLine(koca);
+            //Console.ReadLine();
+
+            /* if (mustafa.Length<10)
+             {
+                 Console.ForegroundColor = ConsoleColor.Green;
+                 Console.WriteLine("kucuk");
+             }
+             else
+             {
+                 Console.ForegroundColor = ConsoleColor.Red;
+                 Console.WriteLine("buyuk");
+             }
+             Console.ReadLine();*/
+
+            //int i;
+            //for ( i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //    Console.ReadLine();
+            //}
+            //int i=0;
+            //while (i<10)
+            //{
+            //    Console.WriteLine(i) ;
+            //    i++;
+            //    Console.ReadLine();
+            //}
+
+            //int i=0;
+            //git:
+            //if (i<10)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //    Console.ReadLine();
+            //        goto git;
+            //}
+            //int i;
+            //Console.WriteLine("sayi gir: ");
+            //i = Convert.ToInt32(Console.ReadLine());
+            //switch (i)
+            //{
+            //    case 1:
+            //        Console.WriteLine("pazartesi");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine( "sali");
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("hicbiri kullanılmadı");
+            //        break;
+            //}
+            //Console.ReadLine();
+        }
+    }
+}
