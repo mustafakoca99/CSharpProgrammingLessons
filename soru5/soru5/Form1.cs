3494b10563b676b09b92980a83425c5b12860896
Mustafa Koca <54147948+mustafakoca99@users.noreply.github.com>
2021-01-16 17:16:54 +0300

    CSharpProgrammingLessons

---
+++ soru5/soru5/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace soru5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+      
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int a = 45;
+            int c = 96;
+            int d = 12;
+            if (c<d)
+            {
+                this.BackColor = Color.Green;
+                label1.Text = "Yeşil";
+            }
+            else if (a>=d && d>c) 
+            {
+                this.BackColor = Color.Yellow;
+                label1.Text = "Sarı";
+            }
+            else if (c>a||a<=d )
+            {
+                this.BackColor = Color.Blue;
+                label1.Text = "Mavi";
+            }
+            else
+            {
+                this.BackColor = Color.Red;
+                label1.Text = "Kırmızı";
+            }
+        }
+
+    }
+}
